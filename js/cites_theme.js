c638816d7eb1db4d24b8230be3cd0d597e390b2c
Cătălin Dogaru <catalin.dogaru@eaudeweb.ro>
2013-08-08 17:23:00 +0300

    [refs #1296] Added the collapsible menu blocks script

---
+++ js/cites_theme.js
@@ -0,0 +1,12 @@
+(function($) {
+  $('document').ready(function() {
+    if ($('#sidebar-first .block-menu').length > 3) {
+      $('#sidebar-first .block-menu').addClass('collapsible');
+      $('#sidebar-first .block-menu .content:not(:first)').hide();
+      $('#sidebar-first .block-menu h2').click(function() {
+        $('#sidebar-first .block-menu .content').not($(this).siblings('.content')).slideUp('fast');
+        $(this).siblings('.content').slideToggle('slow');
+      });
+    }
+  });
+})(jQuery);
