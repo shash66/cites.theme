e8be7d6a9b401e72bd264a2fbc8087b20f0b1487
Cătălin Dogaru <catalin.dogaru@eaudeweb.ro>
2013-08-13 14:38:39 +0300

    [refs #1303] Updated the JS script

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 (function($) {
-  $('document').ready(function() {
-    if ($('#sidebar-first .block-menu').length > 3) {
-      $('#sidebar-first .block-menu').addClass('collapsible');
-      $('#sidebar-first .block-menu .content:not(:first)').hide();
-      $('#sidebar-first .block-menu h2').click(function() {
-        $('#sidebar-first .block-menu .content').not($(this).siblings('.content')).slideUp('fast');
-        $(this).siblings('.content').slideToggle('slow');
-      });
+  Drupal.behaviors.contextualMenu = {
+    attach: function(context, settings) {
+      if ($('#sidebar-first .block-menu').length > 3) {
+        $('#sidebar-first .block-menu').addClass('collapsible');
+        $('#sidebar-first .block-menu .content:not(:first)').hide();
+        $('#sidebar-first .block-menu h2').click(function() {
+          $('#sidebar-first .block-menu .content').not($(this).siblings('.content')).slideUp('fast');
+          $(this).siblings('.content').slideToggle('slow');
+        });
+      }
     }
-  });
+  };
 })(jQuery);
