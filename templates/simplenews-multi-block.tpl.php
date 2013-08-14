e4e1f11f1a3b4b10aa0b37656eb87261a5464c50
Cătălin Dogaru <catalin.dogaru@eaudeweb.ro>
2013-08-14 14:58:16 +0300

    [refs #1303] Updated the simplenews template file

--- templates/simplenews-multi-block.tpl.php
+++ templates/simplenews-multi-block.tpl.php
@@ -1,27 +1,16 @@
 <?php
-// $Id: simplenews-multi-block.tpl.php,v 1.6 2009/01/02 12:01:17 sutharsan Exp $
-
 /**
  * @file
- * Default theme implementation to display the simplenews block.
- *
- * Copy this file in your theme directory to create a custom themed block.
+ * CITES's theme implementation to display the simplenews block.
  *
  * Available variables:
- * - $subscribed: the current user is subscribed to the $tid newsletter
- * - $user: the current user is authenticated
- * - $message: announcement message (Default: 'Stay informed on our latest news!')
+ * - $subscribed: The current user is subscribed to the $tid newsletter.
+ * - $user: The current user is authenticated.
+ * - $message: The announcement message. The default value is "Stay informed on
+ *   our latest news!".
  * - $form: newsletter subscription form
  *
  * @see template_preprocess_simplenews_multi_block()
  */
 ?>
-
-<?php if ($message): ?>
-  <p><?php print $message; ?></p>
-<?php endif; ?>
-
-<?php 
-  $link = l(t('Subscribe to email alerts'), 'newsletter/subscriptions');
-  print render($link);
-?>
+<?php print l(t('Subscribe to email alerts'), 'newsletter/subscriptions'); ?>
